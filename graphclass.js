86a2ffd087a953be90ac3bf396cc63843fec6b87
David Allingham <David.Allingham@newcastle.edu.au>
2021-08-06 17:00:17 +1000

    Updated to use new classes, added methods.

--- graphclass.js
+++ graphclass.js
@@ -7,44 +7,96 @@ class Graph {
   this.name = name;
   this.nodes = [];
   this.edges = [];
-  this.svg = null;
+  this.svg = this.createSvg(); // the SVG group for this graph
+ }
+
+ /*
+   Methods for the Graph class:
+
+   createSvg
+   hide
+   show
+   addNode
+   addNodes
+   removeNode
+   removeNodes
+   numberNodes (wanted?)
+   nameNodes (wanted?)
+   findNode
+   findNodes
+   listNodes
+   randomNode
+   addEdge
+   addEdges
+
+ */
+
+ createSvg(){
+  return appendSvgGroup(this.name,"thesvg");
+ }
+
+ hide(){
+  this.svg.classList.add("hidden");
+ }
+
+ show(){
+  this.svg.classList.remove("hidden");
  }
 
  addNode(name,position,radius){
-  var node = new Node(name,position,radius,this.name,this.nodes.length+1);
-  this.nodes.push(node);
-  return node;
+  this.nodes.push(new Node(name,position,radius,this,this.nodes.length+1));
  }
 
  addNodes(n=1){
   for (var i=0;i<n;i++) this.addNode(randomName(),randomLocation(),randomRadius());
  }
 
+ removeNode(youngest=true){
+  // by default, remove the last-added node
+  if (this.nodes.length){
+   var delnode = (youngest? this.nodes.length-1 : 0);
+   // remove the SVG object
+   this.svg.removeChild(this.nodes[delnode].svg);
+   // then remove this node from the graph
+   this.nodes.splice(delnode,1);
+  }
+ }
+
+ removeNodes(n=1,youngest=true){
+  for (var i=0;i<n;i++) this.removeNode(youngest);
+ }
+
  numberNodes(){
-  for (var i=0;i<this.nodes;i++) this.nodes[i].n = i;
+  for (var i=0;i<this.nodes.length;i++) this.nodes[i].n = i;
  }
 
  nameNodes(){
-  // ...but we also need to sort out the names in the edge list...
-  for (var i=0;i<this.nodes;i++) this.nodes[i].name = "node"+i;
+  // ...but if these change we also need to sort out the edge list...
+  for (var i=0;i<this.nodes.length;i++) this.nodes[i].name = "node"+i;
  }
 
- addEdge(name,fromNode,toNode,svgElement=undefined){
-  var edge = new Edge(name,fromNode,toNode);
-  this.edges.push(edge);
-  return edge;
+ findNode(name){
+  return this.nodes.filter(function(nod){return nod.name==name});
  }
 
- draw(){
-  // create an SVG group for this graph if it does not exist
-  if (this.svg==null){
-   this.svg = appendSvgGroup(this.name,"thesvg");
-   // add subgroups for the nodes and edges
-   appendSvgGroup(this.name+"nodes",this.name);
-   appendSvgGroup(this.name+"edges",this.name);
-  }
-  for (var i=0;i<this.nodes.length;i++) this.nodes[i].draw();
-  for (var i=0;i<this.edges.length;i++) this.edges[i].draw();
+ findNodes(name_regexp){
+  return this.nodes.filter(function(nod){var p=new RegExp("^"+name_regexp+"$","i");return p.test(nod.name)});
+ }
+
+ listNodes(){
+  for (var i=0;i<this.nodes.length;i++) console.log(this.nodes[i].name);
+ }
+
+ randomNode(){
+  return this.nodes[Math.round(Math.random()*this.nodes.length)];
+ }
+
+ addEdge(name,from,to){
+  this.edges.push(new Edge(name,from,to,this));
+ }
+
+ addEdges(n=1){
+  for (var i=0;i<n;i++) this.addEdge(randomName(),this.randomNode(),this.randomNode());
  }
 
 }
@@ -55,7 +107,7 @@ class Node {
   name,
   position,
   radius,
-  graph = null,
+  graph,
   number = -1,
  ){
   this.type = this.constructor.name;
@@ -65,25 +117,27 @@ class Node {
   this.z = (position.length==3? position[2] : 0); // add a default z coordinate if missing
   this.r = radius;
   this.n = number; // number the nodes if they are in a graph
-  this.graph = graph; // name of the parent graph
-  this.svg = null; // placeholder for SVG object which will be created on draw()
+  this.graph = graph; // the parent graph object
+  this.svg = this.createSvg(); // the SVG object for this node
+  this.addToSvgGraph();
  }
 
- draw(){
-  if (this.svg==null){
-   this.svg = new Circle({
-    "fill": "#000",
-    "stroke": "none",
-    "r": this.r,
-    "cx": this.x,
-    "cy": this.y,
-    "z-index": this.z,
-    "id": this.name,
-    "class": "anode",
-   });
-  }
-  var graphnodegroup = document.getElementById(this.graph+"nodes");
-  appendSvgObject(this.svg,graphnodegroup);
+ createSvg(){
+  var c = Circle({
+   "fill": "#000",
+   "stroke": "none",
+   "r": this.r,
+   "cx": this.x,
+   "cy": this.y,
+   "z-index": this.z,
+   "id": this.name,
+   "class": "anode",
+  });
+  return c;
+ }
+
+ addToSvgGraph(){
+  appendSvgObject(this.svg,this.graph.svg);
  }
 
  print(){
@@ -96,16 +150,53 @@ class Node {
 class Edge {
  constructor(
   name,
-  fromNode,
-  toNode,
-  svgElement = undefined,
+  from,
+  to,
+  graph,
+  number = -1,
  ){
   this.type = this.constructor.name;
   this.name = name;
-  if (fromNode.type=="Node") this.from = fromNode;
-  if (toNode.type=="Node") this.to = toNode;
-  this.svgElement = svgElement;
+  this.graph = graph; // the parent graph object
+  this.linewidth = 0.25;
+  this.z = 0;
+
+  // from, to: test that the inputs are Node objects
+  if (from.type!="Node" || to.type!="Node"){
+   console.log("error: edge endpoint is not a Node object");
+   return false;
+  } else {
+   this.from = from;
+   this.to = to;
+  }
+
+  this.svg = this.createSvg(); // the SVG object for this node
+  this.addToSvgGraph();
  }
+
+ print(){
+  console.log("The edge \""+this.name+"\" goes from "+this.from.name+" to "+this.to.name);
+ }
+
+ createSvg(){
+  var L = Line({
+   "stroke": "#000",
+   "stroke-width": this.linewidth,
+   "x1": this.from.x,
+   "y1": this.from.y,
+   "x2": this.to.x,
+   "y2": this.to.y,
+   "z-index": this.z,
+   "id": this.name,
+   "class": "anedge",
+  });
+  return L;
+ }
+
+ addToSvgGraph(){
+  appendSvgObject(this.svg,this.graph.svg);
+ }
+
 }
 
 // automorphism class /////////////////////////////////////////////////////////////////////////////
---
+++ graphclassutils.js
@@ -0,0 +1,32 @@
+/* HTML- and SVG-related functions for the Graph, Node, Edge and Automorphism classes
+   Utility functions which facilitate display and manipulation of Javascript objects on the webpage.
+*/
+
+function primaryGraph(){
+ var g = document.getElementById("thesvg").getAttribute("data-primary-graph");
+ if (g.length) return document.getElementById(g);
+ else return false;
+}
+
+function makePrimary(graph){
+ document.getElementById("thesvg").setAttribute("data-primary-graph",graph.name);
+}
+
+function addGraphNode(graph,position,radius,edgelist=[]){
+ makePrimary(graph);
+ graph.addNode(randomName(),position,radius);
+}
+
+function addRandomGraphNode(graph){
+ makePrimary(graph);
+ graph.addNodes(1);
+}
+
+function addRandomGraphNodes(graph,n=1){
+ makePrimary(graph);
+ graph.addNodes(n);
+}
+
+function removeGraphNodes(graph,n=1){
+ graph.removeNodes(n);
+}
